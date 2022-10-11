e0a63819a2c698717113229bcd68c2196e89676f
greatofdream <greatofdream@gmail.com>
2022-10-11 15:36:03 +0000

    add the limit life result

--- life/tau.py
+++ life/tau.py
@@ -5,8 +5,9 @@ T=10
 N=6.75
 # e=0.3633
 # n=2.446
-n = np.loadtxt(sys.argv[1], skiprows=1, delimiter=',')[1]
+n_limit, n_sensitivity = np.loadtxt(sys.argv[1], skiprows=1, delimiter=',')
 with h5py.File(sys.argv[2], 'r') as ipt:
     e = ipt['eff'][:]['ratio'][-1]
-print(n, e)
-print('{} 10^33'.format(T*N*e/n))
+print(n_limit, n_sensitivity, e)
+print('{} 10^33'.format(T*N*e/n_limit))
+print('{} 10^33'.format(T*N*e/n_sensitivity))
